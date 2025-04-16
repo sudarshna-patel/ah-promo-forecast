c62be9a9e9def906f5c2bb59efa503aa0dc04929
Sudarshna Patel <patel.sudarshna@gmail.com>
2025-04-16 15:22:29 +0200

    test patch logs

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,12 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
+import pytest
+from unittest.mock import patch
 
 
-def test_home():
+@patch("app.main.configure_logs")
+def test_home(mock_logs):
+    from fastapi.testclient import TestClient
+    from app.main import app
+
+    client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200
